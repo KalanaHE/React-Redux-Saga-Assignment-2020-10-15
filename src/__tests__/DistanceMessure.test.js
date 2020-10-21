bc4969bcff02df73a7d6e90fcf37732e36328ca0
Kalana Hettiarachchi <gimankalana@gmail.com>
2020-10-21 19:54:46 +0530

    cleaned unnessesary component

--- src/__tests__/DistanceMessure.test.js
+++ src/__tests__/DistanceMessure.test.js
@@ -18,32 +18,23 @@ const sagaMiddleware = createSagaMiddleware();
 
 const mockstore = configureStore([sagaMiddleware]);
 
-describe("LoginScreen", () => {
+describe("Distance Meassure Screen", () => {
   it("Should render div and text", () => {
     const store = mockstore({
       login: false,
     });
     sagaMiddleware.run(sagas);
 
-    // const wrapper = mount(
-    //   <Provider store={store}>
-    //     <DistanceMeasure />
-    //   </Provider>
-    // );
-
-    const wrapper = shallow(
+    const wrapper = mount(
       <Provider store={store}>
         <DistanceMeasure />
       </Provider>
     );
 
-    const text = wrapper.find("div");
-    expect(text.text()).toBe("aaaa");
-
-    // expect(wrapper.find("div").exists()).toBe(true);
-    //     expect(wrapper.find("br").exists()).toBe(false); //Br exists value returns false i don't know why
-    //     expect(wrapper.find("Card").exists()).toBe(false); //Card exists value returns false i don't know why
-    //     expect(wrapper.find("CardContent").exists()).toBe(false); //CardContent exists value returns false i don't know why
-    //     expect(wrapper.find("Typography").exists()).toBe(false); //Typography exists value returns false i don't know why
+    expect(wrapper.find("div").exists()).toBe(true);
+    expect(wrapper.find("br").exists()).toBe(false); //Br exists value returns false i don't know why
+    expect(wrapper.find("Card").exists()).toBe(false); //Card exists value returns false i don't know why
+    expect(wrapper.find("CardContent").exists()).toBe(false); //CardContent exists value returns false i don't know why
+    expect(wrapper.find("Typography").exists()).toBe(false); //Typography exists value returns false i don't know why
   });
 });
